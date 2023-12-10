fcd71928f72ca806788c4e59a4a458061bf6df7d
Гульназ Иштуганова <spotty1998@mail.ru>
2023-12-10 16:14:24 +0500

    task2

--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
-﻿// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+﻿//Задача 2: Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
 
-int[] array = new int[10];
+int [] array = new int [10];
 int count = 0;
 
 for(int i = 0; i < array.Length; i++)
 {
-    array [i] = new Random().Next(0,101);
-    Console.Write(array[i]+ " ");
-    
-    if (array[i] >=20 && array[i] <=90)
+    array[i] = new Random().Next(0,1001);
+    Console.Write(array[i]+" ");
+}
+Console.WriteLine();
+
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 == 0)
     {
         count++;
     }
 }
-Console.WriteLine();
-Console.Write("Количество элементов массива, значения которых лежат в отрезке [20,90]: "+count);
-
+Console.Write(count);
